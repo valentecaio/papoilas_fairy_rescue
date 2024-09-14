032c4af4bc7c57828aa99ec87d3ce5befcf2d642
valentecaio <cvalente.aio@gmail.com>
2024-09-14 18:05:35 -0300

    tabs

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -5,13 +5,13 @@ extends Camera3D
 @export var distance := 15
 
 func _physics_process(delta: float) -> void:
-  var dir = Input.get_axis("turn_left", "turn_right")
-  if dir:
-    base.rotate(Vector3(0,1,0), deg_to_rad(-dir*delta*50))
+    var dir = Input.get_axis("turn_left", "turn_right")
+    if dir:
+        base.rotate(Vector3(0,1,0), deg_to_rad(-dir*delta*50))
 
 func _on_stage_2d_player_position_changed(cur_player_pos):
-  var angle = deg_to_rad(cur_player_pos.x * 360.0 / 256)
-  position.x = distance * sin(angle)
-  position.z = distance * cos(angle)
-  position.y = (1 - cur_player_pos.y/256) * base.mesh.height
-  look_at(Vector3(0, position.y, 0))
+    var angle = deg_to_rad(cur_player_pos.x * 360.0 / 256)
+    position.x = distance * sin(angle)
+    position.z = distance * cos(angle)
+    position.y = (1 - cur_player_pos.y/256) * base.mesh.height
+    look_at(Vector3(0, position.y, 0))
