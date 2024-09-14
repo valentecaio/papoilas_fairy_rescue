99755df8b3279e28453dbad3e1036a99e39fe1ba
valentecaio <cvalente.aio@gmail.com>
2024-09-14 18:05:20 -0300

    fix last commit

--- scripts/stage_2d.gd
+++ scripts/stage_2d.gd
@@ -5,4 +5,4 @@ extends Node2D
 signal player_position_changed(cur_player_pos)
 
 func _process(delta):
-  emit_signal("player_position_changed", player.position)
+    emit_signal("player_position_changed", player.position)
