1df1f791e4f8565d448fd482027d3f7ec9234eec
valentecaio <cvalente.aio@gmail.com>
2024-09-14 16:31:12 -0300

    finish game

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -1,9 +1,11 @@
 extends Camera3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var base = $"../Tower/Base"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+  # 0 idle, 1 moving right, -1 moving left
+  var dir = Input.get_axis("turn_left", "turn_right")
+
+  if dir:
+    base.rotate(Vector3(0,1,0), deg_to_rad(-dir*delta*50))
