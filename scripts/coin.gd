fc94c5d798a148d709a16f8b5a3e1b8d946e29a9
valentecaio <cvalente.aio@gmail.com>
2024-09-14 12:54:07 -0300

    basic tileset stage

---
+++ scripts/coin.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
+
+func _on_body_entered(body):
+  animation_player.play("pickup")
+  game_manager.add_point()
---
+++ scripts/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+  # gravity.
+  if not is_on_floor():
+    velocity.y += gravity * delta
+
+  # jump
+  if Input.is_action_just_pressed("jump") and is_on_floor():
+    velocity.y = JUMP_VELOCITY
+
+  # 0 idle, 1 moving right, -1 moving left
+  var direction = Input.get_axis("move_left", "move_right")
+
+  # move left/right (and flip animation)
+  if direction:
+    animated_sprite_2d.flip_h = direction < 0
+    velocity.x = direction * SPEED
+  else:
+    velocity.x = move_toward(velocity.x, 0, SPEED)
+
+  # animation
+  if is_on_floor():
+    if direction == 0:
+      animated_sprite_2d.play("idle")
+    else:
+      animated_sprite_2d.play("run")
+  else:
+    animated_sprite_2d.play("jump")
+
+  move_and_slide()
