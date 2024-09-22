45a987848913a66c00d2e3aaf318e6f57771ca44
valentecaio <cvalente.aio@gmail.com>
2024-09-22 15:51:43 -0300

    ladders working

--- scripts/enemies/killzone.gd
+++ scripts/enemies/killzone.gd
@@ -3,11 +3,10 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-    body.get_node("AnimatedSprite2D").play("death")
-    body.get_node("CollisionShape2D").queue_free()
-    body.get_node("CollisionShape2D2").queue_free()
-    Engine.time_scale = 0.5
-    timer.start()
+    if body.has_method("die"):
+        body.die()
+        Engine.time_scale = 0.5
+        timer.start()
 
 func _on_timer_timeout():
     Engine.time_scale = 1
