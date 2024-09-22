da181e72de39ddce3ca803e50e3312f81d8c9ea6
Bruno N. Machado <bnogmac@gmail.com>
2024-09-22 15:18:19 -0300

    Adiciona som de fada e moedas no global

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,17 +1,26 @@
 # Global autoload
 
 extends Node
-
 # width of stage, used to wrap player and enemies around the screen
 const stage_w := 1024
 
 signal open_hatch()
 
-### player stats ###
-var player_coins := 0
+var fairy_soundplayer = preload("res://scenes/fairy_sound.tscn").instantiate()
+var coin_soundplayer = preload("res://scenes/coin_sound.tscn").instantiate()
+
+### player stats ###qe
+var player_coins :
+    set(value):
+        player_coins = value
+        coin_soundplayer.play()
+    get():
+        return player_coins
+        
 var player_fairies :
     set(value):
         player_fairies = value
+        fairy_soundplayer.play()
         if value == 5:
             open_hatch.emit()
     get():
@@ -30,3 +39,7 @@ var player_position: Vector2:
 
 func _ready():
     player_fairies = 0
+    player_coins = 0
+    add_child(fairy_soundplayer)
+    add_child(coin_soundplayer)
+    
